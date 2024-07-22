8ed035d72b4c04a3e43531200e4f874ea4c4a1e3
Nakul <nakulgarg1424@gmail.com>
2024-07-22 11:14:57 +0530

    prefinal 0.2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -60,7 +60,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a href="#"className="text-indigo-400 leading-relaxed">
                 nakulgarg.003@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
